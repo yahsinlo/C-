dee333f1fe9c8e3d41919bd2a8f4a0e5d2edd711
羅雅馨 <yahsin@ccf.org.tw>
2020-06-15 15:46:48 +0800

    commit 1

--- lesson2/lesson2/Program.cs
+++ lesson2/lesson2/Program.cs
@@ -1,23 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace lesson2
 {
+    enum Days {Monday, Tuesday, Wenesday, Thursday, Friday, Saurday, Sunday}
     class Program
     {
         /// <summary>
-        /// Main method 
+        /// Main method
         /// </summary>
         /// <param name="args"></param>
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             int intValue = 0;
             int intValue2 = new int();
-            bool a = true; 
-                  
+            bool a = true;
+
+            Person person = new Person();
+            person.age = 10;
+            Console.WriteLine(person.age);
+            var day = Days.Sunday;
+            Console.WriteLine((int)day);
+            Console.ReadLine();
         }
+    }
+
+    struct Person
+    {
+        public int age;
+        public string name;
+        public string fname;
+        public string lastName;
     }
 }
---
+++ lesson2/lesson3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //object, string, dynamic
+            //System.Object, 
+            object o = new Object();
+            o.GetType();
+            o.ToString();
+            //Console.WriteLine(o.GetType());
+            int i = 5;
+            //Console.WriteLine(i.ToString());
+            String s = "1123";
+            String s2 = "11";
+            s2 += "22";
+            Console.WriteLine(s);
+            Console.WriteLine(s2);
+            Console.WriteLine(s == s2);
+            Console.WriteLine((object)s == (object)s2);
+            char c = s[2];
+            Console.WriteLine(c);
+            String u = "\\\u0066\n";
+            Console.WriteLine(u);
+            string at = @"c:\1234567897891234.cs";
+            Console.WriteLine(at);
+            string at2 = "c:\\aabb1234567897891234.cs";
+            at.Contains("aabb");
+            Console.WriteLine(at2);
+            Console.WriteLine(at2.Length);
+            Console.WriteLine(at2.IndexOf("1234"));
+
+            StringBuilder builder = new StringBuilder();
+            //builder.Append("oooo");
+            //builder.Append("pppp");
+            builder.AppendFormat("hello {0} {1}", "abcde", "world");
+            Console.WriteLine(builder);
+            Console.ReadLine();
+        }
+    }
+}
